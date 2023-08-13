b54751678d09dd5edec26de08fbeea182ed831df
christian <christian.celora@leadsbridge.com>
2023-08-13 10:44:26 +0200

    channel synchronization

--- src/15_channels/channels.go
+++ src/15_channels/channels.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func sendMessage(channel chan string, message string) {
 	channel <- message
+}
+
+func wait(done chan bool) {
+	fmt.Println("Working...")
+	time.Sleep(time.Second)
+	fmt.Println("Done")
+	done <- true
 }
 
 func main() {
@@ -25,11 +35,21 @@ func main() {
 	/**
 	* Because this channel is buffered, we can send these values into the channel without a corresponding concurrent receive.
 	* Notice: i am not using a goroutine for these functions. 
-	* If i don't use it for an unbuffered channel i receive a "fatal error: all goroutines are asleep - deadlock!"
+	* If i don't use it for an unbuffered channel i receive a "fatal error: all goroutines are asleep - deadlock!".
 	*/
 	sendMessage(buffered_channel, "hello")
 	sendMessage(buffered_channel, "world")
-	// These are syncronous, so i will recevie in order. The first print ha the possibility to start with "world"
+	// These are syncronous, so i will recevie in order. The first print ha the possibility to start with "world".
     fmt.Println(<-buffered_channel)
     fmt.Println(<-buffered_channel)
+
+	/** 
+	* Channel Synchronization
+	* 
+	* We can use channels to synchronize execution across goroutines.
+	* When waiting for multiple goroutines to finish, you may prefer to use a WaitGroup.
+	*/
+	done := make(chan bool, 1) // Unbuffered channel would do the same
+	go wait(done)
+	<-done // without this line the program would exit before the goroutine finishes
 }
