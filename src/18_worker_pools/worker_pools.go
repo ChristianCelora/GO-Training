735cfee798aac8173639611c743f446f4e4122e2
christian <christian.celora@leadsbridge.com>
2023-08-17 13:29:10 +0200

    wait group

--- src/18_worker_pools/worker_pools.go
+++ src/18_worker_pools/worker_pools.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func worker(id int, jobs <-chan int, results chan<- int) {
@@ -14,13 +15,20 @@ func worker(id int, jobs <-chan int, results chan<- int) {
 	}
 }
 
+func simple_worker(id int) {
+	fmt.Println("worker", id, "started")
+	time.Sleep(time.Second) // simulate expensive task
+	fmt.Println("worker", id, "finished")
+}
+
 func main() {
+	const n_worker = 3
 	const n_jobs = 5
 	jobs := make(chan int, n_jobs)
 	results := make(chan int, n_jobs)
 
 	// start goroutines. Initially are blocked, because there are no jobs are in the channel
-	for i := 0; i < 3; i++ {
+	for i := 0; i < n_worker; i++ {
 		go worker(i, jobs, results)
 	}
 
@@ -36,5 +44,26 @@ func main() {
 	*/
 	for j := 0; j < n_jobs; j++ {
 		<-results
+	}
+
+	/**
+	* Wait Groups
+	*/
+	var wg sync.WaitGroup
+
+	for i := 0; i < n_worker; i++ {
+		wg.Add(1)
+
+		// Avoid use i inside each goroutine closure. Also passing i to the function as parmeter is fine.
+		idx := i
+
+		go func () {
+			defer wg.Done()
+			// Note: if a WaitGroup is explicitly passed into functions, it should be done by pointer.
+			simple_worker(idx)
+		} ()
+
+		// Block until the WaitGroup counter goes back to 0; all the workers notified they’re done.
+		wg.Wait()
 	}
 }
